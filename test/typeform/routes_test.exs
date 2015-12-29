688285ac16009d67428df41e5650286fcfbdbd0d
Steve Forkin <steve.forkin@gmail.com>
2015-12-29 13:29:00 +1100

    removed some duplication in the routes test

--- test/typeform/routes_test.exs
+++ test/typeform/routes_test.exs
@@ -4,14 +4,17 @@ defmodule Typeform.Routes.Test do
 
   @base_version "latest"
 
-  test "information route" do
+  setup do
     base_url = Typeform.Config.host
+    {:ok, base_url: base_url}
+  end
+
+  test "information route", %{base_url: base_url} do
     information_route = Routes.route(:information)
     assert information_route == "#{base_url}/#{@base_version}"
   end
 
-  test "show form route" do
-    base_url = Typeform.Config.host
+  test "show form route", %{base_url: base_url} do
     show_form_id = 1
     show_form_route = Routes.route(:show_form, form_id: show_form_id)
     assert show_form_route == "#{base_url}/#{@base_version}/forms/#{show_form_id}"
