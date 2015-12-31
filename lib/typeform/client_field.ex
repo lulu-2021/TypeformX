fd8cac4984776add08679080da38efdc52150dba
Steve Forkin <steve.forkin@gmail.com>
2015-12-31 12:16:07 +1100

    added some validation around the form fields that are valid and associated tests

--- lib/typeform/client_field.ex
+++ lib/typeform/client_field.ex
@@ -3,11 +3,34 @@ defmodule Typeform.ClientField do
     ** Summary **
     module contains the struct for the Client Form Field from the API - used for creating a form fields
   """
-
   @derive [Poison.Encoder]
   defstruct [:type, :question]
 
+  @field_types [:short_text, :long_text, :multiple_choice, :picture_choice, :statement, :dropdown, :yes_no,
+                :number, :rating, :opinion_scale, :email, :website, :legal]
+
+  # at compile time we dynamically create a function matching each type
+  @field_types |> Enum.each(fn(ftype) ->
+    def valid_field?(unquote(ftype)), do: true
+  end)
+  # and one to catch all other cases..
+  def valid_field?(_invalid_field), do: false
+
   def build(data) do
-    Map.merge(%Typeform.ClientField{}, %{type: data["type"], question: data["question"]})
+    question_type = data["type"]
+    question_data = data["question"]
+    cond do
+      valid?(question_type) ->
+        Map.merge(%Typeform.ClientField{}, %{type: question_type, question: question_data})
+      true ->
+        {:error, "Invalid Question Type"}
+    end
   end
+
+  defp valid?(data) do
+    question_type = String.to_atom(data)
+    question_type
+    |> valid_field?
+  end
+
 end
---
+++ test/typeform/client_field_test.exs
@@ -0,0 +1,31 @@
+defmodule Typeform.ClientField.Test do
+  use ExUnit.Case
+  alias Typeform.ClientField
+
+  setup do
+    field_types = [:short_text, :long_text, :multiple_choice, :picture_choice, :statement, :dropdown, :yes_no,
+                  :number, :rating, :opinion_scale, :email, :website, :legal]
+    {:ok, field_types: field_types}
+  end
+
+
+  test "should have a correct list of available field types", %{field_types: field_types} do
+    field_types
+    |> Enum.each(fn(field_type) ->
+      assert Typeform.ClientField.valid_field?(field_type)
+    end)
+  end
+
+  test "should not have an incorrect field type" do
+    incorrect_field_type = :short_long_text
+    refute Typeform.ClientField.valid_field?(incorrect_field_type)
+  end
+
+  test "when given an invalid question type - the build function should fail" do
+    invalid_field_data = %{"question" => "What is your name?", "type" => "invalid_question_type"}
+    build_response = invalid_field_data |> Typeform.ClientField.build
+
+    assert build_response == {:error, "Invalid Question Type"}
+  end
+
+end
