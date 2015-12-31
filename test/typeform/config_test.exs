6a043f1c9d37b7b4a865f4f74f29e63595ce1df2
Steve Forkin <steve.forkin@gmail.com>
2015-12-31 21:06:46 +1100

    small test improvement

--- test/typeform/config_test.exs
+++ test/typeform/config_test.exs
@@ -1,6 +1,11 @@
 defmodule Typeform.Config.Test do
   use ExUnit.Case
 
+  setup do
+    Mix.env(:test)
+    {:ok, test_env: Mix.env}
+  end
+
   test "check that we have the test api key" do
     assert Typeform.Config.secret == "test_api_key___"
   end
@@ -8,4 +13,17 @@ defmodule Typeform.Config.Test do
   test "check that we have the test api host" do
     assert Typeform.Config.host == "http://test.lvh.me"
   end
+
+  test "check that we have the dev api key" do
+    Mix.env(:bla)
+    refute Typeform.Config.secret == "test_api_key___"
+    Mix.env(:test)
+  end
+
+  test "check that we have the dev host" do
+    Mix.env(:bla)
+    refute Typeform.Config.host == "http://test.lvh.me"
+    Mix.env(:test)
+  end
+
 end
