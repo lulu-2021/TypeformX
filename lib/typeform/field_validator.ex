2fa095fd7c3dd8aa25380ec13e028e77f68016a5
Steve Forkin <steve.forkin@gmail.com>
2015-12-31 18:29:36 +1100

    added moduledoc to the field validator module

--- lib/typeform/field_validator.ex
+++ lib/typeform/field_validator.ex
@@ -1,4 +1,10 @@
 defmodule Typeform.FieldValidator do
+  @moduledoc """
+    ** Summary **
+
+    module contains common functions for field validation - for now just a valid? function
+    that receives the validate function as a param and first changes the question type to an atom
+  """
 
   def valid?(data, validate_fn) do
     question_type = String.to_atom(data)
