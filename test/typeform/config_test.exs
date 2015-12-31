4b92d36b0bc7e5739f51208da4779700538aff4f
Steve Forkin <steve.forkin@gmail.com>
2015-12-31 20:25:21 +1100

    added another config test

--- test/typeform/config_test.exs
+++ test/typeform/config_test.exs
@@ -1,7 +1,11 @@
 defmodule Typeform.Config.Test do
   use ExUnit.Case
 
-  test "quick check that we have the test api key" do
+  test "check that we have the test api key" do
     assert Typeform.Config.secret == "test_api_key___"
+  end
+
+  test "check that we have the test api host" do
+    assert Typeform.Config.host == "http://test.lvh.me"
   end
 end
